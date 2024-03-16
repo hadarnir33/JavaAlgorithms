369a08a9434fe117f6fda8e599b49e296211a40c
Hadar Nir <hadarnir33@gmail.com>
2024-03-16 16:06:29 +0200

    Add minimalSt function

--- src/Ex2023/Ex2023.java
+++ src/Ex2023/Ex2023.java
@@ -78,6 +78,52 @@ public class Ex2023 {
         return -1;
     }
 
+    /**
+     * Finds the minimal string formed by interleaving the characters of two strings starting from specified indices.
+     * If one of the strings is exhausted, it appends the remaining characters from the other string.
+     *
+     * @param st1 The first string.
+     * @param st2 The second string.
+     * @param i   The index in the first string from which to start interleaving.
+     * @param j   The index in the second string from which to start interleaving.
+     * @return The minimal interleaved string.
+     */
+    public static String minimalSt(String st1, String st2, int i, int j) {
+        // Base case: if both strings are exhausted, return an empty string.
+        if (i >= st1.length() && j >= st2.length()) {
+            return "";
+        }
+
+        // If one string is exhausted, return the remaining characters from the other string.
+        if (i >= st1.length()) {
+            return st2.substring(j);
+        }
+        if (j >= st2.length()) {
+            return st1.substring(i);
+        }
+
+        // If the characters at current indices are the same, include one of them and proceed.
+        if (st1.charAt(i) == st2.charAt(j)) {
+            return st1.charAt(i) + minimalSt(st1, st2, i + 1, j + 1);
+        } else {
+            // If the characters are different, explore both options and choose the shorter one.
+            String minimalStI = st2.charAt(j) + minimalSt(st1, st2, i, j + 1);
+            String minimalStJ = st1.charAt(i) + minimalSt(st1, st2, i + 1, j);
+            return minimalStI.length() < minimalStJ.length() ? minimalStI : minimalStJ;
+        }
+    }
+
+    /**
+     * Finds the minimal string formed by interleaving the characters of two strings.
+     *
+     * @param st1 The first string.
+     * @param st2 The second string.
+     * @return The minimal interleaved string.
+     */
+    public static String minimalSt(String st1, String st2) {
+        // Start interleaving from the beginning of both strings.
+        return minimalSt(st1, st2, 0, 0);
+    }
 
     public static void main(String[] args) {
 
