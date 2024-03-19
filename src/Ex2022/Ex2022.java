d65d9c31dfdb8fe0cb6b0f0872773029a407678a
Hadar Nir <hadartoga@gmail.com>
2024-03-19 20:15:11 +0200

    Add kAlmostSearch function

--- src/Ex2022/Ex2022.java
+++ src/Ex2022/Ex2022.java
@@ -41,7 +41,94 @@ public class Ex2022 {
         return calculateExpression(num, result, maxOp, "3", 3);
     }
 
+    /**
+     * Finds the index of the first positive number to the right of the given index in the array.
+     * @param a The input array.
+     * @param index The index to start searching from.
+     * @return The index of the first positive number, or -1 if not found.
+     */
+    public static int findPositiveNumberFromRight(int[] a, int index) {
+        for (int i = index; i < a.length; i++) {
+            if (a[i] > 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Finds the index of the first positive number to the left of the given index in the array.
+     * @param a The input array.
+     * @param index The index to start searching from.
+     * @return The index of the first positive number, or -1 if not found.
+     */
+    public static int findPositiveNumberFromLeft(int[] a, int index) {
+        while (index >= 0) {
+            if (a[index] > 0) {
+                return index;
+            }
+            index--;
+        }
+        return -1;
+    }
+
+    /**
+     * Searches for a number in an array using k-almost search.
+     * @param a The input array.
+     * @param num The number to search for.
+     * @param low The lower index bound.
+     * @param high The upper index bound.
+     * @return The index of the number if found, or -1 otherwise.
+     */
+    public static int kAlmostSearch(int[] a, int num, int low, int high) {
+        int mid, positiveFromRight, positiveFromLeft;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (a[mid] == num) {
+                return mid;
+            }
+            if (a[mid] == 0) {
+                positiveFromRight = findPositiveNumberFromRight(a, mid + 1);
+                if(positiveFromRight != -1){
+                    if (a[positiveFromRight] == num) {
+                        return positiveFromRight;
+                    }
+                    if (a[positiveFromRight] < num) {
+                        low = positiveFromRight;
+                    }
+                }
+                positiveFromLeft = findPositiveNumberFromLeft(a, mid - 1);
+                if(positiveFromLeft != -1 && low != positiveFromRight){
+                    if (a[positiveFromLeft] == num) {
+                        return positiveFromLeft;
+                    }
+                    if (a[positiveFromLeft] > num) {
+                        high = positiveFromLeft;
+                    } else {
+                        return -1;
+                    }
+                }
+            } else if (a[mid] < num) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Searches for a number in an array using k-almost search.
+     * @param a The input array.
+     * @param num The number to search for.
+     * @return The index of the number if found, or -1 otherwise.
+     */
+    public static int kAlmostSearch(int[] a, int num) {
+        return kAlmostSearch(a, num, 0, a.length - 1);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(calculateExpression(3, 36, 4));
+
     }
 }
